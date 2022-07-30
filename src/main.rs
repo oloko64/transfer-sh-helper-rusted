b6cd64c8b28a6ce170d725e3941dbec578f7016f
OLoKo64 <naldoepingo1@gmail.com>
2022-07-30 12:21:37 -0300

    feat: add base for arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+mod arg_parser;
 mod utils;
-use std::{env, io::{self, Write}, process::exit};
-#[macro_use] extern crate prettytable;
-
-fn execute_version() {
-    println!("\nVersion => v0.1.6\n");
-}
+use std::{
+    env,
+    io::{self, Write},
+    process::exit,
+};
+#[macro_use]
+extern crate prettytable;
 
 fn execute_delete_by_id() {
     println!();
@@ -26,7 +28,7 @@ fn execute_list(del_links: bool) {
     println!();
 }
 
-fn execute_drop () {
+fn execute_drop() {
     utils::delete_database_file();
 }
 
@@ -34,18 +36,23 @@ fn execute_transfer(path: &str) {
     match utils::get_file_size(path) {
         Ok(size) => {
             println!("File size: {}", size);
-        },
+        }
         Err(err) => {
-            println!("{}", err);
+            eprintln!("{}", err);
             exit(1);
         }
     };
     let default_name = path.split('/').last().unwrap_or("Default Name");
     {
         let mut entry_name = String::new();
-        print!("\nEnter the name of the entry (Default name: {}): ", default_name);
+        print!(
+            "\nEnter the name of the entry (Default name: {}): ",
+            default_name
+        );
         io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut entry_name).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut entry_name)
+            .expect("Failed to read line");
         if entry_name.trim().is_empty() {
             entry_name = default_name.to_string();
         }
@@ -57,16 +64,19 @@ fn execute_transfer(path: &str) {
 }
 
 fn execute_warn_upload() {
-    println!("
+    println!(
+        "
     You need to inform a path for the file to upload.
     
     Usage:
     -u, --upload [FILE_PATH]   Upload a new link
-    ");
+    "
+    );
 }
 
 fn execute_help() {
-    println!("
+    println!(
+        "
     Usage:
     -h, --help                 Prints help
     -v, --version              Prints version
@@ -77,27 +87,42 @@ fn execute_help() {
     -D, --drop                 Deletes database file
 
     Obs: You can't send empty files or files above 1.5GB.
-    ");
+    "
+    );
 }
 
 fn main() {
-    utils::create_config_app_folder();
-    utils::create_table();
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        2 => match args[1].as_str() {
-            "-v"  | "--version" => execute_version(),
-            "-l"  | "--list" => execute_list(false),
-            "-L"  | "--listdel" => execute_list(true),
-            "-d"  | "--delete" => execute_delete_by_id(),
-            "-D"  | "--drop" => execute_drop(),
-            "-u"  | "--upload" => execute_warn_upload(),
-            _ => execute_help(),
-        },
-        3 => match args[1].as_str() {
-            "-u" | "--upload" => execute_transfer(&args[2]),
-            _ => execute_help(),
-        },
-        _ => execute_help()
+    let args = arg_parser::prepare_args();
+    if let Some(path) = args.upload {
+        execute_transfer(&path);
+    } else if args.list {
+        execute_list(false);
+    } else if args.list_del {
+        execute_list(true);
+    } else if args.delete {
+        execute_delete_by_id();
+    } else if args.drop {
+        execute_drop();
+    } else {
+        execute_list(false);
     }
+    // utils::create_config_app_folder();
+    // utils::create_table();
+    // let args: Vec<String> = env::args().collect();
+    // match args.len() {
+    //     2 => match args[1].as_str() {
+    //         "-v"  | "--version" => execute_version(),
+    //         "-l"  | "--list" => execute_list(false),
+    //         "-L"  | "--listdel" => execute_list(true),
+    //         "-d"  | "--delete" => execute_delete_by_id(),
+    //         "-D"  | "--drop" => execute_drop(),
+    //         "-u"  | "--upload" => execute_warn_upload(),
+    //         _ => execute_help(),
+    //     },
+    //     3 => match args[1].as_str() {
+    //         "-u" | "--upload" => execute_transfer(&args[2]),
+    //         _ => execute_help(),
+    //     },
+    //     _ => execute_help()
+    // }
 }
