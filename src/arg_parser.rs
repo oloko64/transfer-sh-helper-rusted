74cac03bdb1bc6b0825be869bccd9bd74d9e3274
OLoKo64 <naldoepingo1@gmail.com>
2023-03-28 22:20:13 -0300

    feat: update dependencies

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -2,26 +2,26 @@ use clap::Parser;
 
 /// A simple way to use Transfer.sh from the CLI.
 #[derive(Parser)]
-#[clap(version)]
+#[command(version)]
 pub struct Args {
     /// List all links on database
-    #[clap(short, long, action)]
+    #[arg(short, long, action)]
     pub list: bool,
 
     /// List all delete links on database
-    #[clap(long, short = 'L', action)]
+    #[arg(long, short = 'L', action)]
     pub list_del: bool,
 
     /// Deletes a entry locally as well as from Transfer.sh servers
-    #[clap(short, long, action)]
+    #[arg(short, long, action)]
     pub delete: bool,
 
     /// Delete the database from your system
-    #[clap(long, action)]
+    #[arg(long, action)]
     pub drop: bool,
 
     /// Upload a file to Transfer.sh servers
-    #[clap(short, long, value_parser = validate_path)]
+    #[arg(short, long, value_parser = validate_path)]
     pub upload_file: Option<String>,
 }
 
